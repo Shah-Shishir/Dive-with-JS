04b81ffa74fca1808ed6089dd11d20754ff7bd50
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-06-16 03:15:00 +0600

    Create Math.js

---
+++ Math.js
@@ -0,0 +1,8 @@
+// Math
+
+console.log (Math.random());
+console.log (Math.PI);
+console.log (Math.abs(-2.9));
+console.log (Math.round(-2.9));
+console.log (Math.max(1,2,3,4,5));
+console.log (Math.min(1,2,3,4,5));

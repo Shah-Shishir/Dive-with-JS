1c1ac012fc6c74196e6805427a5110184ea13030
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-06-16 03:57:26 +0600

    Create Strings.js

---
+++ Strings.js
@@ -0,0 +1,32 @@
+// Strings
+
+let message = ' This is a message ';
+
+console.log (1,message.length);
+
+console.log (2,message);
+console.log (3,message.trim());
+
+message = ' This is a message ';
+console.log (4,message);
+console.log (5,message.trimLeft());
+
+message = ' This is a message ';
+console.log (6,message);
+console.log (7,message.trimRight());
+
+console.log (8,message.includes('This'));
+
+console.log (9,message);
+
+console.log (10,message.includes('msg'));
+
+console.log (11,message.startsWith('This'));
+
+console.log (12,message.endsWith('msg'));
+
+console.log (13,message.split(' '));
+
+console.log (14,message.split(''));
+
+console.log (15,message.split('x'));

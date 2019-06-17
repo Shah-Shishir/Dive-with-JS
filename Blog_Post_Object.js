733cc0134ef5d7734518c4aa874046913fea2144
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-06-18 05:31:41 +0600

    Create Blog_Post_Object.js

---
+++ Blog_Post_Object.js
@@ -0,0 +1,13 @@
+let blogPost =  {
+	title : 'a',
+	body : 'b', 
+	author : 'c',
+	views : 10,
+	comments : [
+		{ author : 'a', body : 'b' },
+		{ author : 'c', body : 'd' },
+	],
+	isLive : true
+};
+
+console.log (blogPost);

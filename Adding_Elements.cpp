35419cb9f273fff344114233b6ead34f997c9f4d
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-06-19 02:23:14 +0600

    Create Adding_Elements.cpp

---
+++ Adding_Elements.cpp
@@ -0,0 +1,15 @@
+// Adding Elements
+
+let numbers = [3,4];
+
+// End
+numbers.push (5,6);
+console.log (numbers);
+
+// Front
+numbers.unshift (1,2);
+console.log (numbers);
+
+// Middle
+numbers.splice (2,0,'abc',2.5,'x',567);
+console.log (numbers);

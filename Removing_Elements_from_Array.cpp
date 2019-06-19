027f10f5b23e63048241c9c9332816599117c963
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-06-19 06:29:11 +0600

    Create Removing_Elements_from_Array.cpp

---
+++ Removing_Elements_from_Array.cpp
@@ -0,0 +1,15 @@
+// Removing_Elements_from_Array
+
+let numbers = [1,2,3,4,5,6];
+
+// End
+console.log(numbers.pop());
+console.log(numbers);
+
+// Beginning
+console.log(numbers.shift());
+console.log(numbers);
+
+// Middle
+console.log(numbers.splice(2,2));
+console.log(numbers);

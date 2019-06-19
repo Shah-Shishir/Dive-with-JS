258d523a164d6b6e9948dc3e33a9f7b46c0bf0a9
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-06-19 07:43:01 +0600

    Create Joining_Arrays.js

---
+++ Joining_Arrays.js
@@ -0,0 +1,11 @@
+// Joining_Arrays
+
+let numbers = [1,2,3];
+
+console.log(numbers.join());
+console.log(numbers.join('-'));
+console.log(numbers.join(':'));
+console.log(numbers.join(','));
+
+let words = 'a-b-c-d';
+console.log(words.split('-'));

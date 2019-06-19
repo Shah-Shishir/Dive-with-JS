de5edf73d09cee0a822a19ec32dc4db1150f8f43
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-06-19 06:48:16 +0600

    Create Combining_and_Slicing_Arrays.js

---
+++ Combining_and_Slicing_Arrays.js
@@ -0,0 +1,10 @@
+// Combining_and_Slicing_Arrays
+
+let a1 = [1,2,3];
+let a2 = [4,5,6];
+let combined = a1.concat(a2);
+
+console.log(combined);
+console.log(combined.slice());
+console.log(combined.slice(2,4));
+console.log(combined.slice(2));
